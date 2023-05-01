031424cf998615bd41852ca478714adc08cf45ae
Taha Tuna <tahatuna777@gmail.com>
2023-05-01 20:32:16 +0200

    Initial Commit

---
+++ SwiftUI-Shapes/SwiftUI_ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ShapesApp.swift
+//  SwiftUI-Shapes
+//
+//  Created by Taha Tuna on 1.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
